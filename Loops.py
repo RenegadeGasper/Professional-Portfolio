54e503f3a54a260007ff4a17e835b57456a9ee2b
RenegadeGasper <33296380+RenegadeGasper@users.noreply.github.com>
2022-11-01 18:06:39 -0700

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,6 +1,6 @@
 def forl():
   print("You have chosen a for loop\n")
-  a = input("Create an a value thats less that 10 and it will print out that amount: \n")
+  a = int(input("Create an a value thats less that 10 and it will print out that amount: \n"))
   for a in range(10):
     print('Value of a: ' + a)
 
@@ -8,24 +8,23 @@ def forl():
 def dowhilel():
   print("While Python doesn't have a do while loop, you can reiterate a \n")
   print("You have chosen a for loop\n")
-  x = input("Create an x value thats less that 10 and it will print out that amount: \n")
+  x = int(input("Create an x value thats less that 10 and it will print out that amount: \n"))
   print(x)
   while x < 10:
+    x+=1
     print(x)
 
 
 def whilel():
   print("You have chosen a while loop\n")
   print("Create an i value thats less that 10 and it will print out that amount\n")
-  i = input()
+  i = int(input())
   while i < 10:
     print("Value of i: " + i)
     i += 1
 
+g = int(input("Do you want to do a \nFor loop, type 1\nWhile loop, type 2\nWhile loop, type 3\n"))
 
-print ("Do you want to do a \nFor loop, type 1\nWhile loop, type 2\nWhile loop, type 3\n")
-
-g = input()
 
 if g == 1:
   print ("hi")
@@ -34,4 +33,4 @@ if g == 1:
 if g == 2:
   dowhilel()
 if g == 3:
-  whilel()
+  whilel() 

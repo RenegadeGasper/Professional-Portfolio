0853384cb20e64e180a6cfe8ebc8343d22e2ebc1
RenegadeGasper <33296380+RenegadeGasper@users.noreply.github.com>
2022-11-14 19:47:53 -0700

    be

--- Loops.py
+++ Loops.py
@@ -2,9 +2,8 @@ def forl():
   print("You have chosen a for loop\n")
   a = int(input("Create an a value thats less that 10 and it will print out that amount: \n"))
   for a in range(10):
-    print('Value of a: ' + a)
-
-
+    print("Value of a: " + a)
+    
 def dowhilel():
   print("While Python doesn't have a do while loop, you can reiterate a \n")
   print("You have chosen a for loop\n")
@@ -14,13 +13,12 @@ def dowhilel():
     x+=1
     print(x)
 
-
 def whilel():
   print("You have chosen a while loop\n")
   print("Create an i value thats less that 10 and it will print out that amount\n")
   i = int(input())
   while i < 10:
-    print("Value of i: " + i)
+    str(print("Value of i: " + i))
     i += 1
 
 g = int(input("Do you want to do a \nFor loop, type 1\nWhile loop, type 2\nWhile loop, type 3\n"))
@@ -28,7 +26,6 @@ g = int(input("Do you want to do a \nFor loop, type 1\nWhile loop, type 2\nWhile
 
 if g == 1:
   print ("hi")
-  print(forl())
   forl()
 if g == 2:
   dowhilel()

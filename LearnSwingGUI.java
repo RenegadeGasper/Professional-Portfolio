85dc8c89cc0b99da164f3c0d63b1b2f4a13dbbc9
RenegadeGasper <ikkaku354@gmail.com>
2022-12-11 23:12:41 -0500

    .

--- LearnSwingGUI.java
+++ LearnSwingGUI.java
@@ -5,17 +5,10 @@ import javax.swing.*; //Import the whole Swing Library that includes JFrame, JLa
 >>>>>>> Stashed changes
 import java.awt.*;
 
-<<<<<<< Updated upstream
 public class LearnSwingGUI 
 {
     public static void main(String[] args)
     {
-=======
-public class LearnSwingGUI {
-
-    public static void main(String[] args) {
-//--------- Chapter 1 -- JFrame ------------------------------------------------------------------------------------------
->>>>>>> Stashed changes
         //JFrame is a GUI window to add components into to show interactiveness with the user/coder
 
         JFrame frame = new JFrame(); //Creates a frame, you can also set the title here with double quotation inside the parenthesis
@@ -25,20 +18,12 @@ public class LearnSwingGUI {
         //frame.setResizable(false);     // If false you prevent the frame from being resized
         frame.setVisible(true);  //Make the Frame visible
 
-<<<<<<< Updated upstream
-        JPanel panel =  new JPanel();
-        panel.setBackground(new Color(123, 3, 20));
-
-        ImageIcon image = new ImageIcon("Jean Gulag.png"); //Create an ImageIcon on the top left
-        frame.setIconImage(image.getImage());   //Change icon of the frame
-        frame.getContentPane().setBackground(new Color(123,50,200,90)); //Change color of background
-=======
         /* ImageIcon image = new ImageIcon("Jean Gulag.png"); //Create an ImageIcon on the top left
         frame.setIconImage(image.getImage());   //Change icon of the frame
         frame.getContentPane().setBackground(new Color(215,232,253)); //Change color of background
          */
 //--------- Chapter 2 -- JLabel ------------------------------------------------------------------------------------------       
-        JLabel label = new JLabel(); // Create a label
+        /*JLabel label = new JLabel(); // Create a label
         label.setText("How fat is your shmeat?"); // Set text of label
         frame.add(label); // Adds it manually
 
@@ -55,8 +40,7 @@ public class LearnSwingGUI {
         Border border = BorderFactory.createLineBorder(Color.blue, 3);
         label.setBorder(border);
         label.setVerticalAlignment(JLabel.CENTER); //Set Vertical Position of icon + text within label
-        label.setHorizontalAlignment(JLabel.CENTER); //Set Horizontal Position of icon + text within label 
+        label.setHorizontalAlignment(JLabel.CENTER); //Set Horizontal Position of icon + text within label */
 
->>>>>>> Stashed changes
     }
 }
--- appletdemo.java
+++ appletdemo.java
@@ -1,9 +1,15 @@
-import java.applet.*; 
-impor java.awt.*; 
-public class appletdemo extends Applet 
-{ 
- public void paint(Graphics g) 
- { 
- g.drawString("this is code from java",10,10); 
- } 
-} 
+import java.util.Scanner;
+public class demo {
+    public static void main(String[] args) {
+        int MaxWords = 5;
+
+        Scanner scan = new Scanner(System.in);
+        String word ="";
+    
+        for (int i = 0; i < MaxWords; i++){
+            System.out.println("Please enter word "+(i+1)+" of "+ MaxWords);
+            word = scan.nextLine();
+    
+        }
+        System.out.println(word);
+}
